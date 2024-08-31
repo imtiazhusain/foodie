e67911ca08693fece45e124df43f8aece8ed42d6
Imtiaz Hussain <imtiazhussainsolangi111@gmail.com>
2024-08-31 23:20:14 +0500

    layout file modified

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -2,17 +2,18 @@ import Navbar from "@/components/Navbar";
 import { getCartDataFromApi } from "@/slices/cartSlice";
 import { fetchProducts } from "@/slices/foodListSlice";
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
   useEffect(() => {
     dispatch(fetchProducts());
   }, []);
   useEffect(() => {
-    dispatch(getCartDataFromApi());
-  }, []);
+    if (user) dispatch(getCartDataFromApi());
+  }, [user]);
   return (
     <div className="h-svh md:h-screen flex flex-col  ">
       <Navbar />
