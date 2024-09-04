957b3557238d4534ccc9779cc91caf2830ca8bc2
Imtiaz Hussain <imtiazhussainsolangi111@gmail.com>
2024-09-04 17:14:22 +0500

    frontend .env file and api path functionlaity added

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,7 +1,8 @@
 import { default as axios_lib } from "axios";
 
 const axios = axios_lib.create({
-  baseURL: "http://localhost:2100/api",
+  // baseURL: "http://localhost:2100/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default axios;
