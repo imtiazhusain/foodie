ac1e7b0a24e3f09d3a08fd2c7ca189c8977c9ee6
Imtiaz Hussain <imtiazhussainsolangi111@gmail.com>
2024-09-05 00:51:00 +0500

    code spliting added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,28 @@
-import Login from "./pages/login/Login";
+import { Suspense, lazy } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import Signup from "./pages/signup/Signup";
-import Navbar from "./components/Navbar";
-import Dashboard from "./pages/dashboard/Dashboard.page";
-import PageNotFound from "./pages/page-not-found/PageNotFound.page";
-import Cart from "./pages/cart/Cart.page";
-import PlaceOrder from "./pages/place-order/PlaceOrder.page";
-import ListItems from "./pages/admin/items-list/ListItems.page";
-import AdminLayout from "./pages/admin/Layout";
+
 import Layout from "@/pages/Layout";
-import Orders from "./pages/admin/orders/Orders.page";
-import AddItem from "./pages/admin/add-item/AddItem.page";
-import OrdersStatus from "./pages/placed-orders/PlacedOrders.page";
-import PlacedOrders from "./pages/placed-orders/PlacedOrders.page";
-import VerifyOrder from "./pages/verify-order/VerifyOrder.page";
+import AdminLayout from "./pages/admin/Layout";
 import { useSelector } from "react-redux";
+import { Button } from "./components/ui/button";
+import { Loader2 } from "lucide-react";
+
+// Lazy load the route components
+const Login = lazy(() => import("./pages/login/Login"));
+const Signup = lazy(() => import("./pages/signup/Signup"));
+const Dashboard = lazy(() => import("./pages/dashboard/Dashboard.page"));
+const PageNotFound = lazy(() =>
+  import("./pages/page-not-found/PageNotFound.page")
+);
+const Cart = lazy(() => import("./pages/cart/Cart.page"));
+const PlaceOrder = lazy(() => import("./pages/place-order/PlaceOrder.page"));
+const ListItems = lazy(() => import("./pages/admin/items-list/ListItems.page"));
+const Orders = lazy(() => import("./pages/admin/orders/Orders.page"));
+const AddItem = lazy(() => import("./pages/admin/add-item/AddItem.page"));
+const PlacedOrders = lazy(() =>
+  import("./pages/placed-orders/PlacedOrders.page")
+);
+const VerifyOrder = lazy(() => import("./pages/verify-order/VerifyOrder.page"));
 
 function App() {
   const { user } = useSelector((state) => state.auth);
@@ -22,77 +30,80 @@ function App() {
   console.log(isAdmin);
   return (
     <div className="font-outFit">
-      <Routes>
-        <Route path="/">
-          {/* <Route
-            index
-            element={
-              !user ? <Login /> : isAdmin ? <ListItems /> : <Dashboard />
-            }
-          /> */}
+      <Suspense
+        fallback={
+          <div className="gird place-content-center  h-screen ">loading...</div>
+        }
+      >
+        <Routes>
+          <Route path="/">
+            {!isAdmin && (
+              <Route element={<Layout />}>
+                <Route index element={!user ? <Login /> : <Dashboard />} />
+                <Route
+                  path="login"
+                  element={!user ? <Login /> : <Navigate to="/dashboard" />}
+                />
+                <Route
+                  path="signup"
+                  element={!user ? <Signup /> : <Navigate to="/dashboard" />}
+                />
+                <Route path="cart" element={<Cart />} />
+                <Route path="place-order" element={<PlaceOrder />} />
+                <Route path="dashboard" element={<Dashboard />} />
+                <Route
+                  path="verify-order"
+                  element={user ? <VerifyOrder /> : <Navigate to="/login" />}
+                />
+                <Route
+                  path="placed-orders"
+                  element={user ? <PlacedOrders /> : <Navigate to="/login" />}
+                />
+                <Route path="*" element={<PageNotFound />} />
+              </Route>
+            )}
+            {isAdmin && (
+              <Route element={<AdminLayout />}>
+                <Route index element={!user ? <Login /> : <ListItems />} />
 
-          {!isAdmin && (
-            <Route element={<Layout />}>
-              <Route index element={!user ? <Login /> : <Dashboard />} />
-              <Route
-                path="login"
-                element={!user ? <Login /> : <Navigate to="/dashboard" />}
-              />
-              <Route
-                path="signup"
-                element={!user ? <Signup /> : <Navigate to="/dashboard" />}
-              />
-              <Route path="cart" element={<Cart />} />
-              <Route path="place-order" element={<PlaceOrder />} />
-              <Route path="dashboard" element={<Dashboard />} />
-              <Route
-                path="verify-order"
-                element={user ? <VerifyOrder /> : <Navigate to="/login" />}
-              />
-              <Route
-                path="placed-orders"
-                element={user ? <PlacedOrders /> : <Navigate to="/login" />}
-              />
-              <Route path="*" element={<PageNotFound />} />
-            </Route>
-          )}
-          {isAdmin && (
-            <Route element={<AdminLayout />}>
-              <Route index element={!user ? <Login /> : <ListItems />} />
+                <Route
+                  path="items-list"
+                  element={
+                    user?.role === "Admin" ? (
+                      <ListItems />
+                    ) : (
+                      <Navigate to="/login" />
+                    )
+                  }
+                />
+                <Route
+                  path="orders"
+                  element={
+                    user?.role === "Admin" ? (
+                      <Orders />
+                    ) : (
+                      <Navigate to="/login" />
+                    )
+                  }
+                />
+                <Route
+                  path="add-item"
+                  element={
+                    user?.role === "Admin" ? (
+                      <AddItem />
+                    ) : (
+                      <Navigate to="/login" />
+                    )
+                  }
+                />
+                <Route path="*" element={<PageNotFound />} />
+              </Route>
+            )}
 
-              <Route
-                path="items-list"
-                element={
-                  user?.role === "Admin" ? (
-                    <ListItems />
-                  ) : (
-                    <Navigate to="/login" />
-                  )
-                }
-              />
-              <Route
-                path="orders"
-                element={
-                  user?.role === "Admin" ? <Orders /> : <Navigate to="/login" />
-                }
-              />
-              <Route
-                path="add-item"
-                element={
-                  user?.role === "Admin" ? (
-                    <AddItem />
-                  ) : (
-                    <Navigate to="/login" />
-                  )
-                }
-              />
-              <Route path="*" element={<PageNotFound />} />
-            </Route>
-          )}
-
-          {/* <Route path="profile" element={userData ? <Profile /> : <Login />} /> */}
-        </Route>
-      </Routes>
+            {/* <Route path="profile" element={userData ? <Profile /> : <Login />} /> */}
+          </Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 }
